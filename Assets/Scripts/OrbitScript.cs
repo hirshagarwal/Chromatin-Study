b3aa36d0bb30147616f967799c640e3cc8c123a6
Hirsh R. Agarwal <hirsh.agarwal@gmail.com>
2019-01-24 12:16:31 +0000

    Fix orbit script.

--- Assets/Scripts/OrbitScript.cs
+++ Assets/Scripts/OrbitScript.cs
@@ -21,6 +21,12 @@ public class OrbitScript : MonoBehaviour
     private float x = 0.0f;
     private float y = 0.0f;
 
+    private float _sensitivity = 3f;
+    private Vector3 _mouseReference;
+    private Vector3 _mouseOffset;
+    private Vector3 _rotation;
+    private bool _isRotating;
+
     public bool Menu
     {
         get
@@ -82,18 +88,37 @@ public class OrbitScript : MonoBehaviour
                 //transform.rotation = rotation;
                 //transform.position = position;
 
-                var mx = Input.mousePosition.x - Screen.width / 2;//Input.GetAxis("Mouse X") ;
-                var my = Input.mousePosition.y - Screen.height / 2;//Input.GetAxis("Mouse Y");
+                GameObject objectManager = GameObject.Find("ObjectManager");
+
+                // offset
+                _mouseOffset = (Input.mousePosition - _mouseReference);
+                // apply rotation
+                //_rotation.y = -(_mouseOffset.x + _mouseOffset.y) * _sensitivity;
+                _rotation.y = -(_mouseOffset.x) * _sensitivity;
+                _rotation.x = (_mouseOffset.y) * _sensitivity;
+                // rotate
+                //]
+                //transform.Rotate(_rotation);
+                // transform.eulerAngles += _rotation;
+                transform.RotateAround(objectManager.transform.position, transform.right, -Input.GetAxis("Mouse Y") * _sensitivity);
+                transform.RotateAround(objectManager.transform.position, transform.up, Input.GetAxis("Mouse X") * _sensitivity);
+                // store mouse
+                //transform.rotation = _mouseOffset;
+                Debug.Log("Rotation: " + _mouseOffset);
+                _mouseReference = Input.mousePosition;
+
+                var mx = Input.mousePosition.x - Screen.width / 2;// Input.GetAxis("Mouse X") ;
+                var my = Input.mousePosition.y - Screen.height / 2;// Input.GetAxis("Mouse Y");
 
                 x += mx * xSpeed * distance * 0.02f;
                 y -= my * ySpeed * 0.02f;
-                //  Debug.Log("x " + x + "," + my);
+                // Debug.Log("x " + x + "," + my);
 
 
                 y = ClampAngle(y, yMinLimit, yMaxLimit);
 
-                Quaternion rotation = Quaternion.Euler(y, x, 0);
-                Debug.Log("..." + Input.GetAxis("Mouse ScrollWheel"));
+                Quaternion rotation = Quaternion.Euler(_rotation.y, -_rotation.x, 0);
+                // Debug.Log("..." + Input.GetAxis("Mouse ScrollWheel"));
                 distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
                 //Debug.Log(distance + "," + Input.GetAxis("Mouse ScrollWheel"));
                 RaycastHit hit;
@@ -103,10 +128,11 @@ public class OrbitScript : MonoBehaviour
                 }
                 Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
                 Vector3 position = rotation * negDistance + target.position;
-
-                transform.rotation = rotation;
-                transform.position = position;
+                //Debug.Log("Rotation: " + _rotation.x);
+                //transform.rotation = rotation;
+                //transform.position = position;
             }
+
         }
         else
         {
@@ -114,6 +140,7 @@ public class OrbitScript : MonoBehaviour
             transform.position = new Vector3(0, 0, 1);
             shift = new Vector3(0, 0, 0);
         }
+
         /*
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
@@ -143,7 +170,7 @@ public class OrbitScript : MonoBehaviour
          
         }
         */
-        
+
     }
 
     public static float ClampAngle(float angle, float min, float max)
@@ -153,5 +180,21 @@ public class OrbitScript : MonoBehaviour
         if (angle > 360F)
             angle -= 360F;
         return Mathf.Clamp(angle, min, max);
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse Down");
+        // rotating flag
+        _isRotating = true;
+
+        // store mouse
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        // rotating flag
+        _isRotating = false;
     }
 }
